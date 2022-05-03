c0ec3ddc9ca66044c97406113fb0b3b7d89cdfc3
Souhardya Mandal <souhardya1204@gmail.com>
2022-05-03 23:16:17 +0530

    Added basic login

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,7 @@
 class StaticPagesController < ApplicationController
     def home
-        
+        if session[:user_id]
+            @user = User.find_by(id: session[:user_id])
+        end
     end
 end
