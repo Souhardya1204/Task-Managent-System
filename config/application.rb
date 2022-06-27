2c1fd8a7e3e43cfa41658cc9f60d9a4c419f7efa
Souhardya Mandal <souhardya1204@gmail.com>
2022-06-27 15:35:48 +0530

    Modified some UI

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,6 @@ module TaskManagementSystem
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.active_job.queue_adapter = :sidekiq
+    config.active_storage.replace_on_assign_to_many = false
   end
 end
