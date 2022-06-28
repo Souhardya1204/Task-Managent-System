efeaaa9ce7c568eb5bfa2f1c2918e81f169b859a
Souhardya Mandal <souhardya1204@gmail.com>
2022-06-28 13:03:46 +0530

    Fixed some issues and prepared for deploy

--- app/workers/send_reminder_worker.rb
+++
@@ -1,6 +0,0 @@
-class SendReminderWorker
-    include Sidekiq::Worker
-    def perform(task)
-      ReminderMailer.with(task: task).send_reminder.deliver_later() # calls the mailer we made's method and delivers the email immediately
-    end
-  end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,4 +117,20 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+  config.action_mailer.default_url_options = {
+    host: 'project-task-management-system.herokuapp.com',
+    protocol: 'https'
+  }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  address: 'smtp.gmail.com',
+  port: 587,
+  domain: 'example.com',
+  user_name: ENV['SMTP_USER_NAME'],
+  password: ENV['SMTP_PASSWORD'],
+  authentication: 'plain',
+  enable_starttls_auto: true
+}
 end
