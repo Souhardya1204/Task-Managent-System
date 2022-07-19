14dbaf1b1eb05d6d197ac95344077376a2e9ddd6
Souhardya <souhardya1204.com>
2022-07-19 08:39:14 +0530

    Added enum for status, priority and category

--- app/models/task.rb
+++ app/models/task.rb
@@ -5,6 +5,17 @@ class Task < ApplicationRecord
   validates :employee_id, :name, :priority, presence: true
   has_many_attached :documents
   has_many :subtasks, dependent: :destroy
+  enum status: { Pending: "Pending", "In Progress": "In Progress", Completed: "Completed" }
+  enum priority: { High: 1, Medium: 2, Low: 3 }
+  enum category: { Exams: "Exams", Birthday: "Birthday", Campus: "Campus", "Mock Test": "Mock Test",
+                   Interview: "Interview", Event: "Event", Hiring: "Hiring" }
   scope :my_tasks, -> { where(employee_id: Current.user.id) }
   scope :with_priority, ->(priority) { where("priority = ?", priority) }
+  def self.task_specific_statuses(task)
+    if task.done
+      Task.statuses
+    else
+      Task.statuses.reject { |k, _v| k == "Completed" }
+    end
+  end
 end
