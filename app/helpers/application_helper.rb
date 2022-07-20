9020d4579b22af2ad0f0556b0b7b97e9bf14937a
Souhardya <souhardya1204.com>
2022-07-20 13:15:40 +0530

    Deleted unnecessary files and methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,20 +1,6 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
-  def get_string_priority(task)
-    p = task.priority
-    case p
-    when 1
-      "High"
-    when 2
-      "Medium"
-    when 3
-      "Low"
-    else
-      "Not specified"
-    end
-  end
-
   def category_name(task)
     if task.category
       task.category.name
--- app/models/task.rb
+++ app/models/task.rb
@@ -8,8 +8,6 @@ class Task < ApplicationRecord
   has_many :subtasks, dependent: :destroy
   enum status: { Pending: "Pending", "In Progress": "In Progress", Completed: "Completed" }
   enum priority: { High: 1, Medium: 2, Low: 3 }
-  enum category: { Exams: "Exams", Birthday: "Birthday", Campus: "Campus", "Mock Test": "Mock Test",
-                   Interview: "Interview", Event: "Event", Hiring: "Hiring" }
   scope :my_tasks, -> { where(employee_id: Current.user.id) }
   scope :with_priority, ->(priority) { where("priority = ?", priority) }
   def self.task_specific_statuses(task)
